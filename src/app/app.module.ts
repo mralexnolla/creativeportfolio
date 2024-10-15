7a2a5c4d5bce91638440f973b342641839fc9819
alex <nba4everwanted@gmail.com>
2024-10-14 22:55:25 -0400

    creative portfolio first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContentAreaComponent } from './components/content-area/content-area.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    ContentAreaComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
